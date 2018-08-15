32337d68ae4a549c06bf324c11b1dfb18b8ca417
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 22:13:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+
+  square_array_of_nums =[]
+  
+  array.each do |value_to_square|
+
+    square_array_of_nums << value_to_square * value_to_square
+    
+end
+  square_array_of_nums
 end
